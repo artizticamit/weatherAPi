812a1507ab3bcb3b85f922ee722bff3e482ef44d
Amit Kumar <78965821+artizticamit@users.noreply.github.com>
2021-06-24 14:07:39 +0530

    just a little text update
    Added the text degree in Temperature showing.

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&ap
       let tempValue = data['main']['temp']-273;
       let descValue = data['weather'][0]['description'];
       name.innerHTML  = nameValue;
-      temp.innerHTML = tempValue;
+      temp.innerHTML = tempValue + " Degree";
       desc.innerHTML = descValue;
   })
 
