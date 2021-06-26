b460deff9af3884cb870dd4325832e2115829ac2
Amit Kumar <78965821+artizticamit@users.noreply.github.com>
2021-06-26 10:16:45 +0530

    Added Icon to the website

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ let inputValue = document.querySelector('.inputValue');
 let name = document.querySelector('.name');
 let desc = document.querySelector('.desc');
 let temp = document.querySelector('.temp');
+let img = document.querySelector('.icon');
+let image = document.querySelector('#weather-icon');
 
 
 button.addEventListener('click', function(){
@@ -15,9 +17,12 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&ap
       let nameValue = data['name'];
       let tempValue = data['main']['temp']-273;
       let descValue = data['weather'][0]['description'];
+      let icon = data['weather'][0]['icon'];
       name.innerHTML  = nameValue;
       temp.innerHTML = Math.round(tempValue*100)/100 + " Degree";
       desc.innerHTML = descValue;
+      image.src = "http://openweathermap.org/img/wn/"+icon+"@2x.png";
+      
   })
 
 .catch(err => alert("wrong city name!"))
